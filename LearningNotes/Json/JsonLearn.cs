7a36104b7de4b5f83d8da22bc9c6573f28a7978b
ypeng42 <yqpeng93@gmail.com>
2019-03-01 15:09:21 -0800

    add json example

---
+++ LearningNotes/Json/JsonLearn.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningNotes.Json
+{
+    class JsonLearn
+    {
+        public static void ReadJson()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\yuqing\source\repos\LearningNotes\LearningNotes\Json\Json1.json");
+            var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(text);
+
+            Console.WriteLine(values);
+        }
+    }
+}
--- LearningNotes/Program.cs
+++ LearningNotes/Program.cs
@@ -1,3 +1,4 @@
+using LearningNotes.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,7 @@ namespace LearningNotes
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            JsonLearn.ReadJson();
         }
     }
 }
