3350b7286e14832d476231b1a2ba01a8fa7d0ecb
ypeng42 <yqpeng93@gmail.com>
2019-03-02 21:57:40 -0800

    added ps example

---
+++ LearningNotes/ps/script1.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.Synopsis
+some text here
+
+.Description
+this is a description
+
+.Parameter a
+print something
+
+.Example
+example will only showed up if detailed flag is specified
+#>
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$a
+)
+
+# this is a comment
+echo $a 
