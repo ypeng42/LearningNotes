e1d44ce54800fd846aaddf6fc659a5c231568746
ypeng42 <yqpeng93@gmail.com>
2019-03-10 16:38:16 -0700

    add controller to web project

---
+++ WebProj/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProj.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Year { get; set; }
+
+    }
+}
