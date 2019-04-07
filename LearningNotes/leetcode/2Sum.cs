9ef906b32f96fa34f8eb4658efe81dc5b79e1aea
ypeng42 <yqpeng93@gmail.com>
2019-04-06 21:48:50 -0700

    start doing leetcode

---
+++ LearningNotes/leetcode/2Sum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningNotes.leetcode
+{
+    public class _2Sum
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var rst = new int[2];
+            var reminderToIndex = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (reminderToIndex.TryGetValue(nums[i], out int ind))
+                {
+                    rst = new int[] {i, ind};
+                } else
+                {
+                    //reminderToIndex.Add(target - nums[i], i); // if add the same item twice it will throw exception;
+                    reminderToIndex[target - nums[i]] = i; //use it like a hashmap
+                }
+            }
+
+            return rst;
+        }
+    }
+}
---
+++ LeetcodeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeetcodeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetcodeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5fda59ff-f2a7-43ca-a9e2-7de68a374093")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
