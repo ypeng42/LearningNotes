aa748bbba903696781d778f52b49eda6ed1c335b
ypeng42 <yqpeng93@gmail.com>
2019-04-08 21:21:54 -0700

    reverse string

---
+++ LearningNotes/leetcode/ReverseString.cs
@@ -0,0 +1,25 @@
+﻿namespace LearningNotes.leetcode
+{
+    class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            var start = 0;
+            var end = s.Length - 1;
+
+            while (start < end)
+            {
+                Swap(s, start, end);
+                start++;
+                end--;
+            }
+        }
+
+        public static void Swap(char[] input, int a, int b)
+        {
+            char temp = input[a];
+            input[a] = input[b];
+            input[b] = temp;
+        }
+    }
+}
