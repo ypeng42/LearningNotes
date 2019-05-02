f8ccf59e85e581f31d87e3cbcc1b08375ed24fe8
ypeng42 <yqpeng93@gmail.com>
2019-05-01 22:53:22 -0700

    daily leetcode

---
+++ LearningNotes/leetcode/Array/LIS/NumOfLIS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningNotes.leetcode.Array.LIS
+{
+    /*
+     * 673. Number of Longest Increasing Subsequence(medium)
+     * Input: [1,3,5,4,7]
+    Output: 2
+    Explanation: The two longest increasing subsequence are [1, 3, 4, 7] and [1, 3, 5, 7].
+
+        Input: [2,2,2,2,2]
+    Output: 5
+    Explanation: The length of longest continuous increasing subsequence is 1, and there are 5 subsequences' length is 1, so output 5.
+     */
+    class NumOfLIS
+    {
+        public int FindNumberOfLIS(int[] nums)
+        {
+
+        }
+    }
+}
